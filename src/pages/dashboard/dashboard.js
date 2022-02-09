67073f4987b3f6c9b3b084c6c798c4ffb485bf4b
Dinesh Kumar <mdineshdk21@gmail.com>
2022-02-09 16:20:04 +0530

    unlock update

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,141 +1,148 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./dashboard.css";
-import {Icon} from "@iconify/react";
+import { Icon } from "@iconify/react";
 import Sidebar from '../../components/sidebar';
 
 
 
 const Dashboard = () => {
-    const [Dashboarddata,setDashboarddata]=useState(""); 
-    const [lock,setLock]=useState("unlock");;
-    const [lockerInfo,setLockerInfo]= useState({});
-    const [DashboardInfo,setDashboardInfo]=useState(Dashboarddata);
-    const [searchText, setSearchText]=useState('');
-    const getInfo = async () =>{
-        try{
-            const {data} = await axios.get('http://localhost:3001/locker/lockerdata');
+    const [Dashboarddata, setDashboarddata] = useState("");
+    const [lock, setLock] = useState("unlock");;
+    const [lockerInfo, setLockerInfo] = useState({});
+    const [DashboardInfo, setDashboardInfo] = useState(Dashboarddata);
+    const [searchText, setSearchText] = useState('');
+    const getInfo = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:3001/locker/lockerdata');
             setDashboarddata(data);
-          }catch(err){
-              console.log(err)
-          }
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getInfo();
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         setDashboardInfo(Dashboarddata);
-    },[Dashboarddata]);
+    }, [Dashboarddata]);
 
-    useEffect(()=>{
-        if(searchText==='') {
+    useEffect(() => {
+        if (searchText === '') {
             return;
-        }else{
-        setDashboardInfo(()=>
-        Dashboarddata.filter((item)=>
-        item.name.toLowerCase().match(searchText.toLowerCase())));
+        } else {
+            setDashboardInfo(() =>
+                Dashboarddata.filter((item) =>
+                    item.name.toLowerCase().match(searchText.toLowerCase())));
         }
-    },[searchText,Dashboarddata]);
+    }, [searchText, Dashboarddata]);
 
-    const handleChange =(e) =>{
+    const handleChange = (e) => {
         e.preventDefault();
         setSearchText(e.target.value);
-        if(!e.target.value.length>0){
+        if (!e.target.value.length > 0) {
             setDashboardInfo(Dashboarddata);
         }
     }
 
-    const handleUnlock = async(values)=> {
-    await axios.post("http://localhost:3001/locker/unlock",values)
-    setLock("unlock");
-    console.log(lock);
-       }
-       
+    const handleUnlock = async (values) => {
+        console.log(values)
+        var data = await axios.post("http://localhost:3001/locker/unlockupdate", values)
+        if (data !== null) {
+            setLock("unlock");
+            console.log(lock);
+            getInfo();
+        }
+
+    }
+
     return (
-    <div className='dashboard'>
-        <Sidebar className="dashboard_Sidebar"/>
-        <div className="dashboard_Rightbar">
-            <div className="dashboard_RightbarTop">
-                <Icon icon="iwwa:dashboard" className='dashboard_Icon'/>
-                <h1 className='dashboard_Heading'>Dashboard</h1>
-                <form>
-                <div className='dashboard_Input'>
-                <input
-                className='dashboard_Inputfields'
-                placeholder='Search'
-                name='search'
-                type="text"
-                value={searchText}
-                onChange={handleChange}
-                >
-                </input>
-                <label htmlFor='username' className='dashboard_label_Icon'>
-                <Icon icon="charm:search" />
-                </label>
-                </div>
-                </form>
-            </div>
-            <div className="dashboard_RightbarBottom">
-                <div className="dashboard_lockerContainer">
-                {(()=>{
-                        if(DashboardInfo.length!==0){
-                        return( 
-                        <>   
-                        {DashboardInfo.map((values,key)=>{
-                        return <button className='dashboard_detailget'
-                        onClick={()=>setLockerInfo(values)}>
-                        <div key={key}  className='dashboard_lockerBox'>
-                        <Icon icon="bx:bxs-lock" className='dashboard_lockerIcon' id={values.status}/>
-                            <h5>{values.name}</h5>
-                            <p>{values.status}</p>
-                        {values.status==="lock" ? 
-                        <button type='button' 
-                        className='dashboard_unlockButton'
-                        onClick={()=>handleUnlock(values)}><Icon icon="mdi:toggle-switch-off" className='dashboard_unlockButton_logo'/></button>:""}
+        <div className='dashboard'>
+            <Sidebar className="dashboard_Sidebar" />
+            <div className="dashboard_Rightbar">
+                <div className="dashboard_RightbarTop">
+                    <Icon icon="iwwa:dashboard" className='dashboard_Icon' />
+                    <h1 className='dashboard_Heading'>Dashboard</h1>
+                    <form>
+                        <div className='dashboard_Input'>
+                            <input
+                                className='dashboard_Inputfields'
+                                placeholder='Search'
+                                name='search'
+                                type="text"
+                                value={searchText}
+                                onChange={handleChange}
+                            >
+                            </input>
+                            <label htmlFor='username' className='dashboard_label_Icon'>
+                                <Icon icon="charm:search" />
+                            </label>
                         </div>
-                        </button >
-                        })} 
-                        </>
-                        )}})()}
+                    </form>
                 </div>
-            <div className='dashboard_lockerInfo'>
-                <h3>Occupied locker details</h3>
-                <div className="dashboard_lockerInfoTop">
-                <Icon  icon="bx:bxs-lock" className='dashboard_lockerImg' id={lockerInfo.status}/>
-                <div className="dashboard_locker_details">
-                <p>{lockerInfo.name}</p>
-                <p>{lockerInfo.status}</p>
+                <div className="dashboard_RightbarBottom">
+                    <div className="dashboard_lockerContainer">
+                        {(() => {
+                            if (DashboardInfo.length !== 0) {
+                                return (
+                                    <>
+                                        {DashboardInfo.map((values, key) => {
+                                            return <button className='dashboard_detailget'
+                                                onClick={() => setLockerInfo(values)}>
+                                                <div key={key} className='dashboard_lockerBox'>
+                                                    <Icon icon="bx:bxs-lock" className='dashboard_lockerIcon' id={values.status} />
+                                                    <h5>{values.name}</h5>
+                                                    <p>{values.status}</p>
+                                                    {values.status === "lock" ?
+                                                        <button type='button'
+                                                            className='dashboard_unlockButton'
+                                                            onClick={() => handleUnlock(values)}><Icon icon="mdi:toggle-switch-off" className='dashboard_unlockButton_logo' /></button> : ""}
+                                                </div>
+                                            </button >
+                                        })}
+                                    </>
+                                )
+                            }
+                        })()}
+                    </div>
+                    <div className='dashboard_lockerInfo'>
+                        <h3>Occupied locker details</h3>
+                        <div className="dashboard_lockerInfoTop">
+                            <Icon icon="bx:bxs-lock" className='dashboard_lockerImg' id={lockerInfo.status} />
+                            <div className="dashboard_locker_details">
+                                <p>{lockerInfo.name}</p>
+                                <p>{lockerInfo.status}</p>
+                            </div>
+                        </div>
+                        <div className="dashboard_lockerInfo_details">
+                            <img className="dashboard_profileImg" src="./assets/profile.png" alt="" />
+                            <div className='dashboard_lockerInfo_detailsdiv'>
+                                <h6>First name</h6>
+                                <p></p>
+                            </div >
+                            <div className='dashboard_lockerInfo_detailsdiv'>
+                                <h6>Email</h6>
+                                <p></p>
+                            </div>
+                            <div className='dashboard_lockerInfo_detailsdiv'>
+                                <h6>Phone Number</h6>
+                                <p></p>
+                            </div>
+                            <div className='dashboard_lockerInfo_detailsdiv'>
+                                <h6>IMEI Number</h6>
+                                <p></p>
+                            </div>
+                            <div className='dashboard_lockerInfo_detailsdiv'>
+                                <h6>Login Time </h6>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div className="dashboard_lockerInfo_details">
-                <img className = "dashboard_profileImg" src="./assets/profile.png" alt="" />
-                    <div className='dashboard_lockerInfo_detailsdiv'>
-                    <h6>First name</h6>
-                    <p></p>
-                    </div >
-                    <div className='dashboard_lockerInfo_detailsdiv'>
-                    <h6>Email</h6>
-                    <p></p>
-                    </div>
-                    <div className='dashboard_lockerInfo_detailsdiv'>
-                    <h6>Phone Number</h6>
-                    <p></p>
-                    </div>
-                    <div className='dashboard_lockerInfo_detailsdiv'>
-                    <h6>IMEI Number</h6>
-                    <p></p>
-                    </div>
-                    <div className='dashboard_lockerInfo_detailsdiv'>
-                    <h6>Login Time </h6>
-                    <p></p>
-                    </div>
-                </div>
-            </div>
             </div>
         </div>
-    </div>
     )
 }
 
--- src/pages/superAdminCtrl/adminCtrl.js
+++ src/pages/superAdminCtrl/adminCtrl.js
@@ -1,5 +1,5 @@
-import {useEffect,useState} from 'react';
-import {Icon} from "@iconify/react";
+import { useEffect, useState } from 'react';
+import { Icon } from "@iconify/react";
 import "./adminctrl.css"
 import Sidebar from '../../components/sidebar';
 import useAdminCtrlForm from "./useAdminCtrlForm";
@@ -7,175 +7,176 @@ import validateInfo from './validate';
 import axios from "axios";
 
 const AdminCtrl = () => {
-const [locker,setLocker]=useState("");
-const [Dashboarddata,setDashboarddata]=useState(""); 
-const [DashboardInfo,setDashboardInfo]=useState(Dashboarddata);
-const {values,handleChange,handleSubmit,errors}=useAdminCtrlForm(validateInfo);
+    const [locker, setLocker] = useState("");
+    const [Dashboarddata, setDashboarddata] = useState("");
+    const [DashboardInfo, setDashboardInfo] = useState(Dashboarddata);
+    const { values, handleChange, handleSubmit, errors } = useAdminCtrlForm(validateInfo);
 
-  
-  const getInfo = async () =>{
-      try{
-          const {data} = await axios.get('http://localhost:3001/locker/lockerdata');
-          setDashboarddata(data);
-        }catch(err){
+
+    const getInfo = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:3001/locker/lockerdata');
+            setDashboarddata(data);
+        } catch (err) {
             console.log(err);
         }
-  }
+    }
 
 
-const unlock =async()=>{
-    await axios.post("http://localhost:3001/locker/unlock",DashboardInfo[0]);
-}
+    const unlock = async () => {
+        await axios.post("http://localhost:3001/locker/unlock", DashboardInfo[0]);
+    }
 
-  const unlockAll = async()=>{
-      for(let i=0;i<Dashboarddata.length;i++)
-      {
-        await axios.post("http://localhost:3001/locker/unlock",Dashboarddata[i]);
-      }
-  }
+    const unlockAll = async () => {
+        for (let i = 0; i < Dashboarddata.length; i++) {
+            await axios.post("http://localhost:3001/locker/unlock", Dashboarddata[i]);
+        }
+    }
 
-  useEffect(()=>{
-      getInfo();
-  },[]);
+    useEffect(() => {
+        getInfo();
+    }, []);
 
     return (
         <div className='adminCtrl'>
-        <Sidebar className="adminCtrl_Sidebar"/>
-        <div className="adminCtrl_Rightbar">
-            <div className="adminCtrl_RightbarTop">
-                <Icon icon="carbon:user-settings" className='adminCtrl_Icon'/>
-                <h1 className='adminCtrl_Heading'>Super Admin</h1>
-                <form>
-                    <div className='adminCtrl_search_Input'>
-                    <input
-                    className='adminCtrl_search_Inputfields'
-                    placeholder='Search'
-                    name='username'
-                    type="text"
-                    >
-                    </input>
-                    </div>
-                </form>
-            </div>
-            <div className="adminCtrl_RightbarBottom">
-                <div className="adminCtrl_RightbarBottomleft" >
-                <div className="adminCtrl_lockerContainer">
-                <div className='adminCtrl_lockerContainertop'>
-                <h1>Employee List</h1>
-                <form>
-                <div className='adminCtrl_lockerContainer2_search_Input'>
-                <input
-                className='adminCtrl_lockerContainer2_search_Inputfields'
-                placeholder='Search'
-                name='username'
-                type="text"
-                >
-                </input>
-                <label htmlFor='username' className='adminCtrl_lockerContainer2_search_Icon'>
-                <Icon icon="charm:search" />
-                </label>
+            <Sidebar className="adminCtrl_Sidebar" />
+            <div className="adminCtrl_Rightbar">
+                <div className="adminCtrl_RightbarTop">
+                    <Icon icon="carbon:user-settings" className='adminCtrl_Icon' />
+                    <h1 className='adminCtrl_Heading'>Super Admin</h1>
+                    <form>
+                        <div className='adminCtrl_search_Input'>
+                            <input
+                                className='adminCtrl_search_Inputfields'
+                                placeholder='Search'
+                                name='username'
+                                type="text"
+                            >
+                            </input>
+                        </div>
+                    </form>
                 </div>
-                </form>
+                <div className="adminCtrl_RightbarBottom">
+                    <div className="adminCtrl_RightbarBottomleft" >
+                        <div className="adminCtrl_lockerContainer">
+                            <div className='adminCtrl_lockerContainertop'>
+                                <h1>Employee List</h1>
+                                <form>
+                                    <div className='adminCtrl_lockerContainer2_search_Input'>
+                                        <input
+                                            className='adminCtrl_lockerContainer2_search_Inputfields'
+                                            placeholder='Search'
+                                            name='username'
+                                            type="text"
+                                        >
+                                        </input>
+                                        <label htmlFor='username' className='adminCtrl_lockerContainer2_search_Icon'>
+                                            <Icon icon="charm:search" />
+                                        </label>
+                                    </div>
+                                </form>
+                            </div>
+                            <hr />
+                            <div className='adminctrl_databoard'>
+                                <div className='adminctrl_databoard_top'>
+                                    <label className="adminctrl_label">Open Selected Locker</label>
+                                    <select onChange={(e) => {
+                                        const lock = e.target.value;
+                                        setLocker(lock);
+                                        console.log(lock);
+                                        setDashboardInfo(
+                                            Dashboarddata.filter((item) =>
+                                                item.name.toLowerCase().match(locker.toLowerCase())));
+                                        console.log(DashboardInfo[0]);
+                                    }}>
+                                        {(() => {
+                                            if (Dashboarddata.length !== 0) {
+                                                return (
+                                                    <>
+                                                        {Dashboarddata.map((values, key) => {
+                                                            return <option className="adminctrl_option" key={key} value={values.name}>{values.name}</option>
+                                                        })}
+                                                    </>
+                                                )
+                                            }
+                                        })()}
+                                    </select>
+                                    <button className="adminctrl_Button" type='submit' onClick={unlock}>Open</button>
+                                </div>
+                                <br />
+                                <div className='adminctrl_databoard_bottom'>
+                                    <label className="adminctrl_label">Open All Locker</label>
+                                    <button type="button" className="adminctrl_Button" onClick={unlockAll}>Open</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="adminCtrl_lockerInfoContainer">
+                        <div className='adminCtrl_lockerInfo'>
+                            <div className="adminCtrl_lockerInfotop">
+                                <h3>Locker Occupancy</h3>
+                                <div className='adminCtrl_occupied_lockers'>
+                                    <div className="adminCtrl_occupied_lockers_circular" style={{ background: `conic-gradient(#39B54A ${80 * 3.6}deg,  #c6c6c6 ${80 * 3.6}deg)` }}>
+                                        <div className='adminCtrl_occupied_lockers_value'>80%</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='adminCtrl_lockerInfo2'>
+                            <h3>Add Locker</h3>
+                            <form className='adminCtrl_lockerInfo2_Form' onSubmit={handleSubmit}>
+                                <label htmlFor="">Locker Name</label>
+                                <input
+                                    name="name"
+                                    type="text"
+                                    className='adminCtrl_lockerInfo_Input'
+                                    value={values.name}
+                                    onChange={handleChange} />
+                                {errors.name && <p>{errors.name}</p>}
+                                <br />
+                                <label htmlFor="">Status</label>
+                                <input
+                                    name="status"
+                                    type="text"
+                                    className='adminCtrl_lockerInfo_Input'
+                                    value={values.status}
+                                    onChange={handleChange} />
+                                {errors.status && <p>{errors.status}</p>}
+                                <br />
+                                <label htmlFor="">Subscribe Topic</label>
+                                <input
+                                    name="subscribe_topic"
+                                    type="text"
+                                    className='adminCtrl_lockerInfo_Input'
+                                    value={values.subscribe_topic}
+                                    onChange={handleChange} />
+                                {errors.subscribe_topic && <p>{errors.subscribe_topic}</p>}
+                                <br />
+                                <label htmlFor="">Publish Topic</label>
+                                <input
+                                    name="publish-topic"
+                                    type="text"
+                                    className='adminCtrl_lockerInfo_Input'
+                                    value={values.publish_topic}
+                                    onChange={handleChange} />
+                                {errors.publish_topic && <p>{errors.publish_topic}</p>}
+                                <br />
+                                <label htmlFor="">Message</label>
+                                <input
+                                    name="key"
+                                    type="text"
+                                    className='adminCtrl_lockerInfo_Input'
+                                    value={values.key}
+                                    onChange={handleChange} />
+                                {errors.message && <p>{errors.message}</p>}
+                                <br />
+                                <button className='adminCtrl_lockerInfo_Button'>Add Locker</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <hr />
-                <div className='adminctrl_databoard'>
-                    <div className='adminctrl_databoard_top'>
-                    <label className="adminctrl_label">Open Selected Locker</label>
-                    <select onChange={(e)=>{
-                        const lock=e.target.value;
-                        setLocker(lock);
-                        console.log(lock);
-                        setDashboardInfo(
-                        Dashboarddata.filter((item)=>
-                        item.name.toLowerCase().match(locker.toLowerCase())));
-                        console.log(DashboardInfo[0]);
-                    }}>
-                    {(()=>{
-                    if(Dashboarddata.length!==0){
-                    return( 
-                    <>   
-                    {Dashboarddata.map((values,key)=>{
-                     return <option className="adminctrl_option" key={key} value={values.name}>{values.name}</option>
-                    })}
-                    </>
-                    )}})()}
-                    </select>
-                    <button className="adminctrl_Button" type='submit' onClick={unlock}>Open</button>
-                    </div>
-                    <br/>
-                    <div className='adminctrl_databoard_bottom'>
-                    <label className="adminctrl_label">Open All Locker</label>
-                    <button type="button" className="adminctrl_Button" onClick={unlockAll}>Open</button>
-                    </div>
-                    </div>
-                </div>               
-                </div>
-            <div className="adminCtrl_lockerInfoContainer">
-            <div className='adminCtrl_lockerInfo'>
-                <div className="adminCtrl_lockerInfotop">
-                <h3>Locker Occupancy</h3>
-                <div className='adminCtrl_occupied_lockers'>
-                    <div className="adminCtrl_occupied_lockers_circular"  style={{background:`conic-gradient(#39B54A ${80*3.6}deg,  #c6c6c6 ${80*3.6}deg)`}}>
-                        <div className='adminCtrl_occupied_lockers_value'>80%</div>
-                    </div>
-                </div>
-                </div>                
-            </div>
-            <div className='adminCtrl_lockerInfo2'>
-                <h3>Add Locker</h3>
-                <form className='adminCtrl_lockerInfo2_Form' onSubmit={handleSubmit}>
-                    <label htmlFor="">Locker Name</label>
-                    <input
-                    name="name"
-                    type="text" 
-                    className='adminCtrl_lockerInfo_Input' 
-                    value={values.name}
-                    onChange={handleChange} />
-                    {errors.name && <p>{errors.name}</p>}
-                    <br />
-                    <label htmlFor="">Status</label>
-                    <input 
-                    name="status"
-                    type="text" 
-                    className='adminCtrl_lockerInfo_Input'
-                    value={values.status}
-                    onChange={handleChange} />
-                    {errors.status && <p>{errors.status}</p>}
-                    <br />
-                    <label htmlFor="">Subscribe Topic</label>
-                    <input 
-                    name="subscribe_topic"
-                    type="text" 
-                    className='adminCtrl_lockerInfo_Input'
-                    value={values.subscribe_topic}
-                    onChange={handleChange} />
-                    {errors.subscribe_topic && <p>{errors.subscribe_topic}</p>}
-                    <br />
-                    <label htmlFor="">Publish Topic</label>
-                    <input 
-                    name="publish-topic"
-                    type="text" 
-                    className='adminCtrl_lockerInfo_Input'
-                    value={values.publish_topic}
-                    onChange={handleChange} />
-                    {errors.publish_topic && <p>{errors.publish_topic}</p>}
-                    <br />
-                    <label htmlFor="">Message</label>
-                    <input
-                    name="key" 
-                    type="text" 
-                    className='adminCtrl_lockerInfo_Input'
-                    value={values.key}
-                    onChange={handleChange} />
-                    {errors.message && <p>{errors.message}</p>}
-                    <br />
-                    <button className='adminCtrl_lockerInfo_Button'>Add Locker</button>
-                </form>               
-            </div>
-            </div>
             </div>
         </div>
-    </div>
     )
 }
 
